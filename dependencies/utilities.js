009d07f5d4b2e35f12c44b97843d543977c78246
Alicia Garza <garza.alicia.m@gmail.com>
2024-02-23 20:26:19 -0500

    Updated selector for writeMessage step; fixed 24-hour send count for new installs; new 128px icon

--- dependencies/utilities.js
+++ dependencies/utilities.js
@@ -1,29 +1,25 @@
 async function getSendHistory(increment = false) {
-
-    let { sendHistory } = await browser.storage.local.get(['sendHistory']);
+    const now = new Date();
+    let { sendHistory = [] } = await browser.storage.local.get(['sendHistory']);
     //console.log('sendHistory', sendHistory);
 
-    if (!sendHistory) {
-        return [];
-    }
+    if (sendHistory && sendHistory.length > 0) {
+        for (var i = 0; i < sendHistory.length; i++) {
+            const dateSent = new Date(sendHistory[i]);
+            dateSent.setHours(dateSent.getHours() + 24);
 
-    const now = new Date();
-
-    for (var i = 0; i < sendHistory.length; i++) {
-        const dateSent = new Date(sendHistory[i]);
-        dateSent.setHours(dateSent.getHours() + 24);
-
-        //console.log(`${dateSent.toISOString()} < ${now.toISOString()}`, dateSent < now);
-        if (!(dateSent < now)) {
-            // Items will always be added to the end of the array,so break 
-            // out of the loop when we encounter the first element within 
-            // the 24-hour window; everything else after that will be too
-            break;
+            //console.log(`${dateSent.toISOString()} < ${now.toISOString()}`, dateSent < now);
+            if (!(dateSent < now)) {
+                // Items will always be added to the end of the array,so break 
+                // out of the loop when we encounter the first element within 
+                // the 24-hour window; everything else after that will be too
+                break;
+            }
         }
-    }
 
-    // discard everything prior to element i as it is more than 24 hours old
-    sendHistory = sendHistory.slice(i);
+        // discard everything prior to element i as it is more than 24 hours old
+        sendHistory = sendHistory.slice(i);
+    }
 
     if (increment) {
         sendHistory.push(now.toISOString())
